e4ea3162cec2f0586e8f32d9201b63fb3d1cafe7
Tetsunari Inamura <inamura@nii.ac.jp>
2015-09-25 18:00:13 +0900

    Use standard indent

--- test_server.cpp
+++ test_server.cpp
@@ -5,19 +5,19 @@
 
 int main(void)
 {
-  Connection *to_client = new Connection (CONNECTION_SOCKET | CONNECTION_SERVER | CONNECTION_C);
-  to_client->SetName ("test server");
-  if (to_client->Open ("localhost", NULL, 9000)==CONNECTION_FAIL)
-    {
-      fprintf (stderr, "connect socket error\n");
-      abort();
-    }
-  while (1)
-    {
-      double	data;
-
-      to_client->Receive (CONNECTION_DOUBLE, (gpointer)&data);
-      fprintf (stderr, "received data = %g\n", data);
-    }
-  return 1;
+	Connection *to_client = new Connection (CONNECTION_SOCKET | CONNECTION_SERVER | CONNECTION_C);
+	to_client->SetName ("test server");
+	if (to_client->Open ("localhost", NULL, 9000)==CONNECTION_FAIL)
+		{
+			fprintf (stderr, "connect socket error\n");
+			abort();
+		}
+	while (1)
+		{
+			double	data;
+			
+			to_client->Receive (CONNECTION_DOUBLE, (gpointer)&data);
+			fprintf (stderr, "received data = %g\n", data);
+		}
+	return 1;
 }
